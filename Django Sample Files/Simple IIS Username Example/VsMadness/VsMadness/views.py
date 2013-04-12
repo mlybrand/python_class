e770d29e7e22193161bf3bed8edccd0da5f3d1b0
Mark Lybrand <mlybrand@gmail.com>
2013-04-12 15:46:39 -0700

    Updated the username-meta data access to a variable that exits in multiple Django server platforms. (IIS, and other)

--- Django Sample Files/Simple IIS Username Example/VsMadness/VsMadness/views.py
+++ Django Sample Files/Simple IIS Username Example/VsMadness/VsMadness/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 def home(request):
     t = loader.get_template("index.html")
+	metaUserKey = "LOGON_USER" if "LOGON_USER" in request.META.iterkeys() else "USERNAME"
     c = Context({
-                 "username" : "user: {0}".format(request.META["USERNAME"])
+                 "username" : "user: {0}".format(metaUserKey)
                  })
     return HttpResponse(t.render(c)) 
